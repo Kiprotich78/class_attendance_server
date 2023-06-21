ae599d624577d639605d0899d1cca2148cf4043b
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-21 18:59:00 +0300

    updated database connection

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
 const express = require('express');
 const morgan = require('morgan');
-const dbConnection = require('./db')
+const Mongoose = require('mongoose')
 
 const routers = require('./routes/router');
 
 // Create an Express application
 const app = express();
-const isConnectedToDb = dbConnection();
 
-if(isConnectedToDb){
-    // Middleware
-    app.use(morgan('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-    // Routes
-    app.use('api', routers);
+// Routes
+app.use('api', routers);
 
-    // Start the server
-    const port = process.env.PORT || 5000;
-    app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-    });
-}
+const startServer = async () => {
+    try {
+        await Mongoose.connect('mongodb+srv://kiprotich:WhRcgpv1fKIGPSaX@class-register.fzaugrk.mongodb.net/?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to the database');
 
+        // Start the server
+        const port = process.env.PORT || 5000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+
+    } catch (error) {
+        console.error('Database connection error:', error);
+        // Exit the application if the database connection fails
+        process.exit(1);
+    }
+};
+
+startServer();
