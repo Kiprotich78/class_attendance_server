415d19852d1bef4a91407434a68eba549640e559
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-23 11:30:12 +0300

    modified models to allow authentication

--- models/lecturerModel.js
+++ models/lecturerModel.js
@@ -1,25 +1,33 @@
-const mongoose = require('mongoose');
+const Mongoose = require('mongoose');
+const { Schema } = Mongoose
 
-const lecturerSchema = new mongoose.Schema({
+const lecturerSchema = new Schema({
   firstName: {
     type: String,
     required: true,
+    minlength: 2,
+    maxlength: 50
   },
   lastName: {
     type: String,
     required: true,
+    minlength: 2,
+    maxlength: 50
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    maxlength: 100,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   },
   password: {
     type: String,
     required: true,
+    minlength: 6
   },
 });
 
-const lecturer = mongoose.model('lecturer', lecturerSchema);
+const lecturer = Mongoose.model('lecturer', lecturerSchema);
 
 module.exports = lecturer;
---
+++ models/studentModel.js
@@ -0,0 +1,46 @@
+const Mongoose = require('mongoose');
+const { Schema } = Mongoose;
+
+const studentSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    registrationNumber: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 100,
+        match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    },
+    phone: {
+        type: Number,
+        required: true,
+        min: 1000000000,
+        max: 9999999999
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+        default: 'Other'
+    }
+});
+
+const StudentModel = Mongoose.model('Student', studentSchema);
+
+module.exports = StudentModel;
