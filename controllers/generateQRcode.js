5509618b9b752aa7c725e4bf6c4d0678d41cb5b9
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-29 18:23:31 +0300

    add validation in qrcode generator

--- controllers/generateQRcode.js
+++ controllers/generateQRcode.js
@@ -1,13 +1,37 @@
 const QrCode = require('qrcode')
-
+const UnitModel = require('../models/unitModel');
+const LessonModel = require('../models/lessonModel')
+const { mongoose } = require('mongoose');
 
 const generateQRcode = async (req, res)=>{
     try {
         const unitID = req.query.unitId; 
         const lessonID = req.query.lessonId; 
 
+        let unitId, lessonId;
+
+        try{
+            unitId = new mongoose.Types.ObjectId(unitID)
+            lessonId = new mongoose.Types.ObjectId(lessonID)
+
+        }
+        catch(error){
+            console.log(error)
+            return res.status(400).json({Error: "Unit id or lesson id is invalid"});
+        }
+
         if(!unitID || !lessonID){
             return res.status(400).json({Error: "Both unit id and lesson id are required"})
+        }
+
+        const existingUnit = await UnitModel.findById({_id: unitId});
+        if(!existingUnit){
+            return res.status(400).json({Error: "Unit not available in our database"})
+        }
+
+        const existingLesson = await LessonModel.findById({_id: lessonId});
+        if(!existingLesson){
+            return res.status(400).json({Error: "Lesson not available in our database"})
         }
 
         const jsonData = {unitID, lessonID};
@@ -22,8 +46,8 @@ const generateQRcode = async (req, res)=>{
         });
 
     } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
+        console.log(err)
+        res.status(500).send({Error: 'Internal Server Error'});
     }
 }
 
