b861e28405af1f8beeee26984deb19beb6ad5ea2
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-06-30 14:31:19 +0300

    enabled cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const Mongoose = require('mongoose')
+const cors = require('cors');
 require('dotenv').config();
 
 const routers = require('./routes/router');
@@ -9,6 +10,7 @@ const routers = require('./routes/router');
 const app = express();
 
 // Middleware
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
